1f8dc117175467f7e088777b2c6742ad9acef6ab
Alex Alvarez <a01207765@itesm.mx>
2018-03-03 19:33:13 +0000

    Agregar Lab16 y script de bases de datos Tienda.sql

---
+++ Lab16/login.php
@@ -0,0 +1,24 @@
+
+<?php
+    session_start();
+    require_once("modelo.php");
+    
+    if(isset($_SESSION["usuario"]) ) {
+        include("_header.html");
+        $user = $_SESSION["usuario"];
+        include("_logged_view.html");
+        include("_footer.html"); 
+    }else if (login($_POST["usuario"], $_POST["password"]) ) {
+        unset($_SESSION["error"]);
+        $_SESSION["usuario"] = $_POST["usuario"];
+        $user = $_SESSION["usuario"];
+        include("_header.html");
+        include("_logged_view.html");
+        include("_cuestionario.html");
+        include("_footer.html"); 
+    } else if($_POST["usuario"]!="alex" || $_POST["password"]!="1234") {
+        $_SESSION["error"] = "Usuario y/o contraseña incorrectos";
+        header("location: index.php");
+    }
+    
+?>
